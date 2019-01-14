3413032d198f9c5400c01b2272ea1e54a54635bb
cdbdev <39485622+cdbdev@users.noreply.github.com>
2019-01-14 20:39:49 +0100

    Update my_arch_install.sh

--- script/my_arch_install.sh
+++ script/my_arch_install.sh
@@ -9,16 +9,6 @@
 # ----------------------------------------------------------------------- #
 
 
-
-# Check if user is root 
-# --------------------- 
-
-if [ $EUID -ne 0 ]; then
-   echo "This script must be run as root" 1>&2
-   exit 1
-fi
-
-
 # Initial setup ( keyboard, wireless, time/date )
 # ----------------------------------------------- 
 
