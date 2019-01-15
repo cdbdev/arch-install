56452edb50e1b7ca10caf56d8019a56c3a2a5e7c
cdbdev <39485622+cdbdev@users.noreply.github.com>
2019-01-15 08:49:37 +0100

    Update my_arch_install.sh

--- script/my_arch_install.sh
+++ script/my_arch_install.sh
@@ -108,7 +108,7 @@ hwclock --systohc
 # 3 Localization
 echo "Setup Localization"
 # 3.1 uncomment 'en_US.UTF-8 UTF-8'
-sed -i '/#en_US.UTF-8/s/^#//' /etc/locale.gen
+echo "en_US.UTF-8 UTF-8" > /etc/locale.gen
 locale-gen
 echo "LANG=en_US.UTF-8" > /etc/locale.conf
 echo "KEYMAP=be-latin1" > /etc/vconsole.conf
