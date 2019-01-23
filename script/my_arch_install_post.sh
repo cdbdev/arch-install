2f37aee75a2f24136e94fd486d0bdd308c734d33
cdbdev <39485622+cdbdev@users.noreply.github.com>
2019-01-23 20:03:28 +0100

    Update my_arch_install_post.sh

--- script/my_arch_install_post.sh
+++ script/my_arch_install_post.sh
@@ -5,11 +5,11 @@ echo ":: Installing light-locker..."
 sudo yes | pacman -S light-locker xfce4-power-manager --noconfirm
 xfconf-query -c xfce4-session -p /general/LockCommand -s "light-locker-command --lock" --create -t string
 echo ":: Installing blueman..."
-sudo yes | pacman -S blueman --noconfirm
+yes | sudo pacman -S blueman --noconfirm
 sudo systemctl enable bluetooth
 sudo mv /home/"$USER"/90-blueman.rules /etc/polkit-1/rules.d/
 echo ":: Enable dual boot with windows..."
-sudo yes | pacman -S ntfs-3g --noconfirm
+yes | sudo pacman -S ntfs-3g --noconfirm
 sudo mkdir /mnt/windows
 sudo mount /dev/sda3 /mnt/windows
 sudo yes | pacman -S os-prober --noconfirm
