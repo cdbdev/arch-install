a852356ac5c454dc78a29185e447f437206fb1e4
cdbdev <39485622+cdbdev@users.noreply.github.com>
2019-01-25 21:59:23 +0100

    Update my_arch_install_post.sh

--- script/my_arch_install_post.sh
+++ script/my_arch_install_post.sh
@@ -12,7 +12,7 @@ echo ":: Enable dual boot with windows..."
 yes | sudo pacman -S ntfs-3g --noconfirm
 sudo mkdir /mnt/windows
 sudo mount /dev/sda3 /mnt/windows
-sudo yes | pacman -S os-prober --noconfirm
+yes | sudo pacman -S os-prober --noconfirm
 sudo os-prober
 sudo grub-mkconfig -o /boot/grub/grub.cfg
 sudo umount /mnt/windows
