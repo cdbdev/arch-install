5f3ec6d68cffac73dfbb2a9f63bf2b2f67d7f142
cdbdev <39485622+cdbdev@users.noreply.github.com>
2019-10-08 22:08:40 +0200

    Update my_arch_install_post.sh

--- script/my_arch_install_post.sh
+++ script/my_arch_install_post.sh
@@ -12,13 +12,6 @@
 echo ":: Running post-installation..."
 
 # ----------------------------------------------- 
-# Add screenfetch
-# ----------------------------------------------- 
-
-echo ":: Adding screenfetch..."
-echo screenfetch >> .bashrc
-
-# ----------------------------------------------- 
 # Disable XFCE buttons on shutdown
 # ----------------------------------------------- 
 echo ":: Disable buttons on shutdown..."
@@ -26,36 +19,9 @@ xfconf-query -c xfce4-session -np '/shutdown/ShowSuspend' -t 'bool' -s 'false'
 xfconf-query -c xfce4-session -np '/shutdown/ShowHibernate' -t 'bool' -s 'false'
 
 # ----------------------------------------------- 
-# Discard unused packages weekly
-# ----------------------------------------------- 
-echo ":: Discard unused packages weekly"
-sudo systemctl enable paccache.timer
-
-# ----------------------------------------------- 
 # Install light locker
 # ----------------------------------------------- 
-echo ":: Installing light-locker..."
-yes | sudo pacman -S light-locker --noconfirm
+echo ":: Enabling light-locker..."
 xfconf-query -c xfce4-session -p /general/LockCommand -s "light-locker-command --lock" --create -t string
-
-# ----------------------------------------------- 
-# Install bluetooth package and add config
-# ----------------------------------------------- 
-echo ":: Installing blueman..."
-yes | sudo pacman -S blueman --noconfirm
-sudo systemctl enable bluetooth
-sudo mv /home/"$USER"/90-blueman.rules /etc/polkit-1/rules.d/
-
-# ----------------------------------------------- 
-# Enable dual boot with windows
-# ----------------------------------------------- 
-echo ":: Enable dual boot with windows..."
-yes | sudo pacman -S ntfs-3g --noconfirm
-sudo mkdir /mnt/windows
-sudo mount /dev/sda3 /mnt/windows
-yes | sudo pacman -S os-prober --noconfirm
-sudo os-prober
-sudo grub-mkconfig -o /boot/grub/grub.cfg
-sudo umount /mnt/windows
 
 echo ":: Post-installation finished"
